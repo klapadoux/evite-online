db55d45a98ad51663e18a7ade36bd6a3bb5cd313
klapadoux <klapadoux@hotmail.com>
2020-11-10 22:57:32 -0500

    Les joueurs sont des curseurs

--- client/assets/scripts/client.js
+++ client/assets/scripts/client.js
@@ -1,29 +1,75 @@
-// const randomColor = require('randomcolor');
-
 (() => {
   const sock = io()
   
-  const create_player_avatar = () => {
-    
+  let thisPlayer;
+  const players = {}
+  
+  const Player = (args) => {
+    return {
+      args,
+      color: '#000',
+      x: 0,
+      y: 0,
+      node: null,
+      
+      createNode() {
+        this.node = document.createElement('div')
+        this.node.classList.add('player')
+        this.node.style.backgroundColor = this.color
+        this.node.style.top = this.y + 'px'
+        this.node.style.left = this.x + 'px'
+      },
+      
+      moveTo({x, y}) {
+        this.y = x
+        this.y = y
+        this.node.style.top = y + 'px'
+        this.node.style.left = x + 'px'
+      },
+      
+      getEmitParams() {
+        return {
+          color: this.color,
+          x: this.x,
+          y: this.y,
+        }
+      },
+      
+      init() {
+        const {color, x, y} = args
+        this.color = color
+        this.x = x
+        this.y = y
+        this.createNode()
+        
+        return this
+      }
+    }.init()
   }
   
-  sock.on('init_player', text => {
-    // console.log( randomColor() );
+  const addPlayerToGame = (player) => {
+    document.body.append(player.node)
+    players[player.color] = player
+  }
+  
+  
+  sock.on('init_player', args => {
+    thisPlayer = Player(args)
+    addPlayerToGame(thisPlayer)
   })
   
-  sock.on('mousemove', pos => {
-    console.log( pos );
-    const serverDot = document.createElement('div')
-    serverDot.classList.add('server-dot')
-    serverDot.style.top = pos.y + 'px'
-    serverDot.style.left = pos.x + 'px'
-    document.body.append(serverDot)
+  sock.on('mousemove', playerArgs => {
+    if ( 'undefined' === typeof players[playerArgs.color] ) {
+      addPlayerToGame(Player(playerArgs))
+    }
+    
+    players[playerArgs.color].moveTo(playerArgs) 
   })
   
   window.addEventListener('mousemove', event => {
-    sock.emit('mousemove', {
-      x: event.pageX,
-      y: event.pageY,
-    })
+    thisPlayer.x = event.pageX
+    thisPlayer.y = event.pageY
+    
+    sock.emit('mousemove', thisPlayer.getEmitParams())
   })
 })()
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
+const randomColor = require('randomcolor')
 
 const app = express()
 
@@ -10,9 +11,13 @@ const server = http.createServer(app)
 const io = socketio(server)
 
 io.on('connection', sock => {
-  sock.emit('init_player', 'Salut dude.')
+  sock.emit('init_player', {
+    color: randomColor(),
+    x: 300,
+    y: 300,
+  })
   
-  sock.on('mousemove', pos => io.emit('mousemove', pos))
+  sock.on('mousemove', playerParams => io.emit('mousemove', playerParams))
 })
 
 server.on('error', error => {
