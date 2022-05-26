4ac292520291f2410ba24f513a39f5c95bf60a9e
klapadoux <klapadoux@hotmail.com>
2022-05-26 08:17:04 -0400

    Stylise form

--- client/assets/scripts/client.js
+++ client/assets/scripts/client.js
@@ -4,26 +4,6 @@ import Player from './player.js'
 const socket = io()  
 let userGame = null
 
-const appendPlayerForm = () => {
-  const form = document.createElement('form')
-  form.id = 'user-info'
-  
-  const input = document.createElement('input')
-  input.setAttribute('name')
-  
-  form.append(input)
-  document.body.append(form)
-  
-  form.addEventListener('submit', (event) => {
-    event.preventDefault()
-    
-    socket.emit('user_is_ready_to_play', {
-      id: socket.id,
-      name: document.querySelector('input').value
-    })
-  })
-}
-
 socket.once('connect', () => {
   const form = document.getElementById('user-info-form')
   form.addEventListener('submit', (event) => {
@@ -33,6 +13,9 @@ socket.once('connect', () => {
       id: socket.id,
       name: document.querySelector('input').value
     })
+    
+    const section = document.querySelector('.user-section')
+    section.classList.add('user-section--disabled')
   })
   
   socket.emit('get_game', game => {
