54ecb0500e4b20b550c2972e56f741610660a16b
klapadoux <klapadoux@hotmail.com>
2022-06-04 22:58:54 -0400

    Fix settings and link style

--- client/assets/scripts/utils.js
+++ client/assets/scripts/utils.js
@@ -63,7 +63,7 @@ export const Utils = {
       })
     }
     
-    return {x, y}
+    return { x, y }
   },
   
   /**
--- server/settings.js
+++ server/settings.js
@@ -1,7 +1,7 @@
 module.exports = {
   // DEBUG
   SHOW_SPAWN_RECT: false,
-  INVINCIBLE_PLAYERS: true,
+  INVINCIBLE_PLAYERS: false,
   
   // GAME CONTROL
   MAX_OBJECTIVES_AT_ONCE: 6,
